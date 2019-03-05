41b507ae5a311b1f167d5a1571765873273de4ff
Brian Kalish <bkalish42@gmail.com>
2019-03-05 13:36:04 -0500

    Color Changing (Blue)
    Blue's rigged up and colorchange should now work for all colors

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Anima2D;
+using UnityEditor;
 
 public class ColorChange : MonoBehaviour
 {
@@ -10,10 +12,13 @@ public class ColorChange : MonoBehaviour
     public Material red;
     public Material yellow;
     public Material green;
+
+    public GameObject[] colorMeshes;
     // Start is called before the first frame update
     void Start()
     {
         playerRenderer = GetComponent<Renderer>();
+        colorMeshes[1].SetActive(true);
     }
 
     // Update is called once per frame
@@ -25,20 +30,24 @@ public class ColorChange : MonoBehaviour
     public void ChangeBlue()
     {
         playerRenderer.material = blue;
+        colorMeshes[1].SetActive(true);
     }
 
     public void ChangeRed()
     {
         playerRenderer.material = red;
+        colorMeshes[2].SetActive(true);
     }
 
     public void ChangeYellow()
     {
         playerRenderer.material = yellow;
+        colorMeshes[3].SetActive(true);
     }
 
     public void ChangeGreen()
     {
         playerRenderer.material = green;
+        colorMeshes[4].SetActive(true);
     }
 }
